34d6c47948e8a5fdea4299275ffe28b397057380
unknown <hitalo.aurelio@hotmail.com>
2022-11-26 18:07:23 -0300

    adicionado arquivo env_gen novo

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -11,7 +11,7 @@ chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 CONFIG_STRING = """
 DEBUG=True
 SECRET_KEY= "django-insecure-(@6wgpd2n@@ltot25f=6y%@*xfi!wegjjmo+0u+$j1%ko)ou5n"
-ALLOWED_HOSTS=127.0.0.1, .localhost, https://vaquejada-teste.herokuapp.com
+ALLOWED_HOSTS=https://vaquejada-teste.herokuapp.com
 """.strip() % get_random_string(50, chars)
 
 # Writing our configuration file to '.env'
